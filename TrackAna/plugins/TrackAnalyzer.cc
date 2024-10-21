de537a0a8755948102a242b14abf7892a6bd1d87
vinayakrishnan <vinaykrishnanmb@gmail.com>
2024-10-21 09:52:41 +0200

    track associated jets

--- TrackAna/plugins/TrackAnalyzer.cc
+++ TrackAna/plugins/TrackAnalyzer.cc
@@ -30,6 +30,14 @@
 #include "FWCore/Utilities/interface/InputTag.h"
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/JetReco/interface/PFJet.h"
+#include "DataFormats/JetReco/interface/PFJetCollection.h"
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/SiStripDetId/interface/SiStripDetId.h"
+#include "DataFormats/TrackerRecHit2D/interface/SiStripRecHit1D.h"
+#include "DataFormats/TrackerRecHit2D/interface/SiStripRecHit2D.h"
+#include "DataFormats/TrackerRecHit2D/interface/TrackerSingleRecHit.h"
+
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "TH1.h"
@@ -51,6 +59,7 @@
 // This will improve performance in multithreaded jobs.
 
 using reco::TrackCollection;
+//using reco::PFJetCollection;
 
 class TrackAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources> {
 public:
@@ -66,19 +75,32 @@ private:
   void SetVarToZero();
   // ----------member data ---------------------------
   edm::EDGetTokenT<reco::TrackCollection> tracksToken_;  //used to select what tracks to read from configuration file
+  edm::EDGetTokenT<reco::PFJetCollection> jetToken_;
+
+
   //edm::EDGetTokenT<reco::BeamSpot> beamSpotToken_;
   
   TTree* trackTree;
+
   edm::Service<TFileService> fs;
 
   edm::EventNumber_t eventN;
   const static int nMax = 40000000;
 
   uint16_t nTracks;
-  
+  int nJets;
+
   int runN;
   int lumi;
 
+  int charge_lowpt;
+  int charge_highpt;
+  int avgcharge_lowpt;
+  int avgcharge_highpt;
+  int width_lowpt;
+  int width_highpt;
+  
+  
   float trkPt[nMax];
   float trkEta[nMax];
   float trkPhi[nMax];
@@ -87,21 +109,26 @@ private:
   float trkDz1[nMax];
   float trkDzError1[nMax];
   unsigned char trkAlgo[nMax];
-  uint8_t trkNHit[nMax];
-  uint8_t trkNdof[nMax];
+  int  trkNHit[nMax];
+  int  trkNdof[nMax];
   uint8_t trkNlayer[nMax];
 
   float trkChi2[nMax];
+  float trkNChi2[nMax];
   float trkPtError[nMax];
+  
+  float jetPt[nMax];
+  float jetEta[nMax];
+  float jetPhi[nMax];
+  float jetMass[nMax];
+
   
 };
 
 
 TrackAnalyzer::TrackAnalyzer(const edm::ParameterSet& iConfig){
-//: tracksToken_(consumes<TrackCollection>(iConfig.getUntrackedParameter<edm::InputTag>("tracks"))) {
-  
   tracksToken_ = consumes<reco::TrackCollection>(iConfig.getParameter<edm::InputTag>("tracks"));
-  //beamSpotToken_ = consumes<reco::BeamSpot>(iConfig.getParameter<edm::InputTag>("beamSpot"));
+  jetToken_   = consumes<reco::PFJetCollection>(iConfig.getParameter<edm::InputTag>("jets"));
     
   usesResource("TFileService");
 
@@ -109,8 +136,8 @@ TrackAnalyzer::TrackAnalyzer(const edm::ParameterSet& iConfig){
   trackTree->Branch("event", &eventN, "event/i");
   trackTree->Branch("run",   &runN, "run/I");
   trackTree->Branch("lumi",  &lumi, "lumi/I");
-  trackTree->Branch("nTracks",  &nTracks, "nTracks/I");
 
+  trackTree->Branch("nTracks",  &nTracks, "nTracks/I");
   trackTree->Branch("trkPt",  trkPt, "trkPt[nTracks]/F");
   trackTree->Branch("trkEta",  trkEta, "trkEta[nTracks]/F");
   trackTree->Branch("trkPhi",  trkPhi, "trkPhi[nTracks]/F");
@@ -121,12 +148,27 @@ TrackAnalyzer::TrackAnalyzer(const edm::ParameterSet& iConfig){
   trackTree->Branch("trkDzError1",  trkDzError1, "trkDzError1[nTracks]/F");
   
   trackTree->Branch("trkChi2",  trkChi2, "trkChi2[nTracks]/F");
+  trackTree->Branch("trkNChi2",  trkNChi2, "trkNChi2[nTracks]/F");
   trackTree->Branch("trkPtError",  trkPtError, "trkPtError[nTracks]/F");
 
   trackTree->Branch("trkAlgo",trkAlgo,"trkAlgo[nTracks]/s");
   trackTree->Branch("trkNHit",trkNHit,"trkNHit[nTracks]/I");
   trackTree->Branch("trkNdof",trkNdof,"trkNdof[nTracks]/I");
   trackTree->Branch("trkNlayer",trkNlayer,"trkNlayer[nTracks]/I");
+  
+  trackTree->Branch("nJets",  &nJets, "nJets/I");
+  trackTree->Branch("jetPt",  jetPt, "jetPt[nJets]/F");
+  trackTree->Branch("jetEta",  jetEta, "jetEta[nJets]/F");
+  trackTree->Branch("jetPhi",  jetPhi, "jetPhi[nJets]/F");
+  trackTree->Branch("jetMass",  jetMass, "jetMass[nJets]/F");
+
+  // cluster variable
+  trackTree->Branch("charge_lowpt",&charge_lowpt,"charge_lowpt/I");
+  trackTree->Branch("charge_highpt",&charge_highpt,"charge_highpt/I");
+  trackTree->Branch("width_lowpt",&width_lowpt,"width_lowpt/I");
+  trackTree->Branch("width_highpt",&width_highpt,"width_highpt/I");
+  trackTree->Branch("avgcharge_lowpt",&avgcharge_lowpt,"avgcharge_lowpt/I");
+  trackTree->Branch("avgcharge_highpt",&avgcharge_highpt,"avgcharge_highpt/I");
 }
 
 TrackAnalyzer::~TrackAnalyzer() {
@@ -142,27 +184,20 @@ void TrackAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
   eventN = iEvent.id().event();
   lumi = iEvent.id().luminosityBlock();
   const auto& tracksHandle = iEvent.getHandle(tracksToken_);
-
+  const auto& jetHandle = iEvent.getHandle(jetToken_);
   
-  // edm::Handle<reco::TrackCollection> tracks;
-  // iEvent.getByToken(tracksToken_, tracks);
-
-  // edm::Handle<reco::BeamSpot> beamSpot;
-  // iEvent.getByToken(beamSpotToken_, beamSpot);
-
   if (!tracksHandle.isValid()) {
     edm::LogError("TrackAnalyzer") << "No valid track collection found";
+    return;
+  }
+  if (!jetHandle.isValid()) {
+    edm::LogError("TrackAnalyzer") << "No valid jet collection found";
     return;
   }
   
   // Retrieve the actual product from the handle
   const reco::TrackCollection& tracks = *tracksHandle;
-
-  // if (!beamSpot.isValid()) {
-  //   edm::LogError("TrackAnalyzer") << "No valid beam spot found";
-  //   return;
-  // }
- 
+  const reco::PFJetCollection& jets = *jetHandle;
   
   for (const auto& track : tracks) {
 
@@ -177,15 +212,78 @@ void TrackAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
     trkDxyError1[nTracks]= track.dxyError();
     trkDzError1[nTracks] = track.dzError();
     trkPtError[nTracks] = track.ptError();
-    trkChi2[nTracks] = track.normalizedChi2();
+    trkChi2[nTracks] = track.chi2();
+    trkNChi2[nTracks] = track.normalizedChi2();
     trkNdof[nTracks] = track.ndof();
     trkAlgo[nTracks] = track.algo();
     trkNHit[nTracks] = track.numberOfValidHits();
     trkNlayer[nTracks] = track.hitPattern().trackerLayersWithMeasurement();
     nTracks++;
+
+   
+    for (auto const &hit : track.recHits()){
+      if (!hit->isValid())
+	continue;
+      DetId detid = hit->geographicalId();
+      int subDet = detid.subdetId();
+      
+      bool hitInStrip = (subDet == SiStripDetId::TIB) || (subDet == SiStripDetId::TID) || (subDet == SiStripDetId::TOB) || (subDet == SiStripDetId::TEC);
+      
+      if (hitInStrip){
+      	const std::type_info &type = typeid(*hit);
+      	if (type == typeid(SiStripRecHit1D)){
+	  
+      	  const SiStripRecHit1D *striphit = dynamic_cast<const SiStripRecHit1D *>(hit);
+	  if(striphit != nullptr){    
+	    SiStripRecHit1D::ClusterRef stripclust(striphit->cluster());
+	    if (track.pt() < 0.72 && track.chi2()/track.ndof() < 3){
+	      charge_lowpt = stripclust->charge();
+	      width_lowpt  = stripclust->size();
+	      avgcharge_lowpt = (charge_lowpt+width_lowpt/2)/width_lowpt;
+	      
+	    }
+	    if (track.pt() > 0.72 && track.chi2()/track.ndof() < 3){
+	      charge_highpt = stripclust->charge();
+	      width_highpt  = stripclust->size();
+	      avgcharge_highpt = (charge_highpt+width_highpt/2)/width_highpt;
+	    }
+      	  }
+	}
+	if (type == typeid(SiStripRecHit2D)){
+	  
+      	  const SiStripRecHit2D *striphit2D = dynamic_cast<const SiStripRecHit2D *>(hit);
+	  if(striphit2D != nullptr){    
+	    SiStripRecHit2D::ClusterRef stripclust2D(striphit2D->cluster());
+	    if (track.pt() < 0.72 && track.chi2()/track.ndof() < 3){
+	      charge_lowpt = stripclust2D->charge();   
+	      width_lowpt  = stripclust2D->size();
+	      avgcharge_lowpt = (charge_lowpt+width_lowpt/2)/width_lowpt;
+	    }
+	    if (track.pt() > 0.72 && track.chi2()/track.ndof() < 3){
+	      charge_highpt = stripclust2D->charge();
+	      width_highpt  = stripclust2D->size();
+	      avgcharge_highpt = (charge_highpt+width_highpt/2)/width_highpt;
+	    }
+      	  }
+      	}
+      }
+      //++charge;
+      trackTree->Fill();
+    }
+   
   }
 
-  trackTree->Fill();
+  for (const auto& jet : jets) {
+    if (nJets >= nMax) {
+      continue;
+    }
+    jetPt[nJets] = jet.pt();
+    jetEta[nJets] = jet.eta();
+    jetPhi[nJets] = jet.phi();
+    jetMass[nJets] = jet.mass();
+    nJets++;
+  }
+  //trackTree->Fill();
 
   
 #ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
@@ -209,6 +307,13 @@ void TrackAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& description
 void TrackAnalyzer::SetVarToZero()
 {
   nTracks = 0;
+  nJets = 0;
+  charge_lowpt = 0;
+  charge_highpt = 0;
+  width_lowpt = 0;
+  width_highpt = 0;
+  avgcharge_lowpt = 0;
+  avgcharge_highpt = 0;
   for (int i = 0; i < nMax; ++i) {
     trkPt[i] = 0;
     trkEta[i] = 0;
@@ -219,6 +324,14 @@ void TrackAnalyzer::SetVarToZero()
     trkDzError1[i] = 0;
     trkPtError[i] = 0;
     trkChi2[i] = 0;
+    trkNChi2[i] = 0;
+    trkNdof[i]=0;
+  }
+  for (int i=0; i < nMax; ++i){
+    jetPt[i] = 0;
+    jetEta[i] = 0;
+    jetPhi[i] = 0;
+    jetMass[i] = 0;
     
   }
 }
--- TrackAna/python/Trackanalyzer_cfg.py
+++ TrackAna/python/Trackanalyzer_cfg.py
@@ -13,13 +13,15 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
 
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(11))
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        'file:/eos/home-v/vmuralee/PREanalysis/raw/Muon_outputPhysicsHIPhysicsRawRECOPrime0.root' # File for Tracks from SiStripCluster
+        #'file:../../NTuplizer/step_raw_RAW2DIGI_L1Reco_RECO.root'
+        'file:/scratchssd/muraleed/PREanalysis/rereco_ntuples/Muon_RecoRawPrime0Int16bit.root'
+        #'file:/scratchssd/muraleed/PREanalysis/rereco_ntuples/Muon_RecoRawPrime0Int8bit.root' # File for Tracks from SiStripCluster
         #'file:/eos/home-v/vmuralee/PREanalysis/raw/step2_dump_Rawprime.root'  # File for Tracks from ApproxSiStripCluster
     )
 )
@@ -28,17 +30,18 @@ process.source = cms.Source("PoolSource",
 process.offlineBeamSpot = cms.EDProducer("BeamSpotProducer")
 
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, '140X_dataRun3_HLT_v3', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '140X_dataRun3_Prompt_v3', '')
 
 process.trackAnalyzer = cms.EDAnalyzer('TrackAnalyzer',
-                                       tracks = cms.InputTag('generalTracks'),
-                                       #beamSpot = process.offlineBeamSpot#cms.InputTag('offlineBeamSpot')
+                                       tracks = cms.InputTag('generalTracks','','reRECO'),
+                                       jets = cms.InputTag('ak4PFJets','','reRECO')
+                                       
 )
 process.outputTracks = cms.OutputModule("PoolOutputModule",
     compressionAlgorithm = cms.untracked.string('ZLIB'),
     compressionLevel = cms.untracked.int32(7),
     eventAutoFlushCompressedSize = cms.untracked.int32(31457280),
-    fileName = cms.untracked.string('file:/eos/home-v/vmuralee/PREanalysis/outputFiles/flatTracktest.root'),
+    fileName = cms.untracked.string('file:/scratchssd/muraleed/PREanalysis/flat_ntuples/Flat_RawPrimeIntTrackTest.root'),
     outputCommands = cms.untracked.vstring(
     'drop *',   
     'keep *_*SiStripClusters2ApproxClusters*_*_*',
@@ -46,6 +49,6 @@ process.outputTracks = cms.OutputModule("PoolOutputModule",
     )
 )
 
-process.TFileService = cms.Service("TFileService", fileName=cms.string('/eos/home-v/vmuralee/PREanalysis/outputFiles/flatTracktest.root'))
+process.TFileService = cms.Service("TFileService", fileName=cms.string('/scratchssd/muraleed/PREanalysis/flat_ntuples/Flat_TrackTest.root'))
 
 process.p = cms.Path(process.trackAnalyzer)
